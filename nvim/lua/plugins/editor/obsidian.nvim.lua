1e50311abddcecf6abf4b0e58275e3a72167d2de
chawiwi <aisawa15@gmail.com>
2026-02-25 21:24:49 -0800

    add obsidian/trouble

---
+++ nvim/lua/plugins/editor/obsidian.nvim.lua
@@ -0,0 +1,26 @@
+-- Plugin: obsidian-nvim/obsidian.nvim
+-- Installed via store.nvim
+
+return {
+	{
+		"obsidian-nvim/obsidian.nvim",
+		version = "*", -- use latest release, remove to use latest commit
+		ft = "markdown",
+		---@module 'obsidian'
+		---@type obsidian.config
+		opts = {
+			legacy_commands = false, -- this will be removed in the next major release
+			workspaces = {
+				{
+					name = "personal",
+					path = "~/vaults/personal",
+				},
+				{
+					name = "work",
+					path = "~/vaults/work",
+				},
+			},
+		},
+	},
+}
+
---
+++ nvim/lua/plugins/ui/trouble.nvim.lua
@@ -0,0 +1,9 @@
+-- Plugin: folke/trouble.nvim
+-- Installed via store.nvim
+return {
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+	},
+}
