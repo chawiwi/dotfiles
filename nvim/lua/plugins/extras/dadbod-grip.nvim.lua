daf4c05860abee7594bd5f14470614d153770ac0
chawiwi <aisawa15@gmail.com>
2026-03-14 21:28:40 -0700

    dadbod grip

---
+++ nvim/lua/plugins/extras/dadbod-grip.nvim.lua
@@ -0,0 +1,11 @@
+-- Plugin: joryeugene/dadbod-grip.nvim
+-- Installed via store.nvim
+
+return {
+    {
+        "joryeugene/dadbod-grip.nvim",
+        dependencies = {
+            "tpope/vim-dadbod"
+        },
+    },
+}
