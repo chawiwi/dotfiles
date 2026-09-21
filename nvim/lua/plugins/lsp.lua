b47e3c4e1d6eddd88000c147948dcdf3b8416bb4
Kevin Aisawa <aisawa15@gmail.com>
2026-09-21 11:16:44 -0700

    Update mason auto install

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,14 +2,31 @@ vim.pack.add({
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
 })
 
-vim.lsp.enable({
+local mason_tools = {
+	"bash-debug-adapter",
 	"bash-language-server",
-	"ty",
+	"debugpy",
 	"json-lsp",
 	"lua-language-server",
 	"marksman",
+	"prettierd",
+	"ruff",
+	"shfmt",
+	"stylua",
+	"ty",
+	"yaml-language-server",
+}
+
+vim.lsp.enable({
+	"bash-language-server",
+	"json-lsp",
+	"lua-language-server",
+	"marksman",
+	"ruff",
+	"ty",
 	"yaml-language-server",
 })
 
@@ -23,6 +40,11 @@ vim.lsp.config("lua-language-server", {
 })
 
 require("mason").setup()
+
+require("mason-tool-installer").setup({
+	ensure_installed = mason_tools,
+	run_on_start = true,
+})
 
 require("nvim-treesitter").setup({
 	install_dir = vim.fn.stdpath("data") .. "/site",
